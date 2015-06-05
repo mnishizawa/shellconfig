83edfb6357f122e09769d2452d9594e1d1b0d3f1
Michael Nishizawa <michael.nishizawa@spectrumhealth.org>
2015-06-05 16:11:31 -0400

    initial commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+# brew install basics
+brew install tmux zsh python
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh 
+
+git clone https://github.com/powerline/powerline.git
+pip install --user --editable ./powerline
+
+ln -s themes/nishizawa.zsh-theme ~/.oh-my-zsh/themes
