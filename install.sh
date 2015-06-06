bdb762dd09b49d9eb43b4b26f3ada15c84754a45
Mike Nishizawa <michael@fortitude-solutions.com>
2015-06-06 14:37:46 -0400

    before applying tmux-powerline

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 # brew install basics
 brew install tmux zsh python reattach-to-user-namespace
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh 
 
-git clone https://github.com/powerline/powerline.git
-pip install --user --editable ./powerline
+if [ ! -d ~/.oh-my-zsh]; then
+ curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh 
+fi
 
-ln -s themes/nishizawa.zsh-theme ~/.oh-my-zsh/themes
+if [ ! -d ./powerline ]; then
+  git clone https://github.com/powerline/powerline.git
+  pip install --user --editable ./powerline
+fi
+
+if [ ! -d ./tmux-powerline ]; then
+  git clone git@github.com:erikw/tmux-powerline.git
+fi
+
+ln -fs $(pwd)/themes/nishizawa.zsh-theme ~/.oh-my-zsh/themes
+ln -fs $(pwd)/dotfiles/zshrc ~/.zshrc
+ln -fs $(pwd)/dotfiles/tmux.conf ~/.tmux.conf
