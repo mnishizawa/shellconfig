b24e25dc67edf6c0e6466800d2c2a8f04da5750e
Michael Nishizawa <michael.nishizawa@spectrumhealth.org>
2015-07-06 12:43:11 -0400

    fixed install script and added environment variable for zsh theme.

--- install.sh
+++ install.sh
@@ -1,19 +1,14 @@
 # brew install basics
 brew install tmux zsh python reattach-to-user-namespace
 
-if [ ! -d ~/.oh-my-zsh]; then
+if [ ! -d ~/.oh-my-zsh ]; then
  curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh 
 fi
 
-if [ ! -d ./powerline ]; then
-  git clone https://github.com/powerline/powerline.git
-  pip install --user --editable ./powerline
-fi
+git submodule update powerline
+git submodule update tmux-powerline 
 
-if [ ! -d ./tmux-powerline ]; then
-  git clone git@github.com:erikw/tmux-powerline.git
-fi
-
-ln -fs $(pwd)/themes/nishizawa.zsh-theme ~/.oh-my-zsh/themes
+# ln -fs $(pwd)/themes/nishizawa.zsh-theme ~/.oh-my-zsh/themes
 ln -fs $(pwd)/dotfiles/zshrc ~/.zshrc
 ln -fs $(pwd)/dotfiles/tmux.conf ~/.tmux.conf
+ln -fs $(pwd)/dotfiles/tmux-powerlinerc ~/.tmux-powerlinerc
